55a91a3e08e19000bd719c6e296a24110ef5cc7d
Danish-d1 <shaikhdanish3019@gmail.com>
2024-07-31 23:26:25 -0400

    Day19 Regular Expressions

---
+++ Day19-Regular-Expressions/regular-expression.js
@@ -0,0 +1,11 @@
+const regexx = /hello/g;
+const text = 'hello hello hi everyone my name is Danish'
+console.log(text.replace(regexx, "HELLO"));
+
+
+//Task no 1 -
+
+const regex = /JavaScript/g;
+const regularExpression = 'Hi Everyone lets start JavaScript and learn JavaScript deeply';
+const str = regex.test(regularExpression);
+console.log(str);
